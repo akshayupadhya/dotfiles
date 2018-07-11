90316d4907e83046dd5dde12a04b0524b99095f7
akshay upadhya <akshaysrinagar@gmail.com>
2018-07-11 14:55:40 +0530

    mysql and php

---
+++ mysql.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install mysql-server
+mysql_secure_installation
---
+++ php.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
